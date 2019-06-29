d56876c101b2a7821a7f01960d1ded4a40309989
rohit-roy <rohit.roy2393@gmail.com>
2019-06-29 23:25:07 +0530

    dist file updated

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.js']
   },
   output: {
-    filename: './formatNumerals.bundle.js',
+    filename: './index.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
